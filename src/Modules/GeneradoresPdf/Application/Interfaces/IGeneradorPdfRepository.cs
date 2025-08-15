88728d345e682e11e1c8458c3735351caf98bcef
EduardoCastellanosP <eduardoeliascastellanospicon@gmail.com>
2025-08-15 13:01:45 -0500

    feat: :tada: Estructuracion Carpetas Proyecto

---
+++ src/Modules/GeneradoresPdf/Application/Interfaces/IGeneradorPdfRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proyectoC_.src.Modules.GeneradoresPdf.Application.Interfaces
+{
+    public interface IGeneradorPdfRepository
+    {
+        
+    }
+}
---
+++ src/Modules/Usuarios/Domain/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proyectoC_.src.Modules.Usuarios.Domain.Entities
+{
+    public class Usuario
+    {
+        
+    }
+}
