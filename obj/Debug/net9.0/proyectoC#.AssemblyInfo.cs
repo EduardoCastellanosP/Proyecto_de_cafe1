2bb06ee82d7efe20f07be65bda37b7badb1582b6
EduardoCastellanos <eduardoeliascastellanospicon@gmail.com>
2025-08-16 12:45:15 -0500

    fix: :bug: fix bugs

--- obj/Debug/net9.0/proyectoC#.AssemblyInfo.cs
+++ obj/Debug/net9.0/proyectoC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c963e99bae866319ed1feeea80ac163bc1c240ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48eeee6805a504717748fc53fba0f8487f2c5add")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
