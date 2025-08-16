b49f0dc9f8b2ba487f6ef26722c31b721acea67e
EduardoCastellanos <eduardoeliascastellanospicon@gmail.com>
2025-08-16 09:32:08 -0500

    feat: :sparkles: MenuVariedades

--- obj/Debug/net9.0/proyectoC#.AssemblyInfo.cs
+++ obj/Debug/net9.0/proyectoC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2f2dc4b4af3743b108f9bbed344e0814bad06d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792e3f95e1f54ebabc8bcc6c96e28e82e406c08c")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
